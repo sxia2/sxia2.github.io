bc58e1a59efe64bd24c2c5fa55ff99213f3fc31f
sxia2 <sxia2@ncsu.edu>
2022-01-02 01:15:34 -0600

    pattern

--- projects_data/AutoPatternmaker/main.js
+++ projects_data/AutoPatternmaker/main.js
@@ -1,65 +1,110 @@
-document.getElementById("upload").addEventListener("change", upload, false);
-document.getElementById("download").addEventListener("click", download, false);
+// document.getElementById("Armstrong").addEventListener("click", upload, false);
+// document.getElementById("download").addEventListener("click", download, false);
 var out = "";
-function upload(e) {
+var data = "";
+var filename = "";
+var method = "";
+var DXF = "";
+function filename_inital(pattern_mentod) {
+    if (pattern_mentod == 'Armstrong') {
+        filename = 'Armstrong.csv'
+        method = 'armstrong'
+    } else if (pattern_mentod == 'Others') {
+        filename = 'test.csv'
+        method = 'Others'
+
+    } else {
+        console.log('error in method...')
+    }
+    upload();
+}
+function upload() {
     document.getElementById('csvForm').innerHTML = "";
+    d3.csv(filename, function (data) {
+        for (var i = 0; i < data.length; i++) {
+            // console.log(data[i].Name);
+            // console.log(data[i].Age);
+            out = document.getElementById('csvForm').innerHTML
+            out += '<label for="fname">' + data[i].id + ': </label>';
+            out += '<input size="4" value="' + data[i].value + '">';
+            out += '<br>';
+            document.getElementById('csvForm').innerHTML = out;
+        }
+    });
+}
+function draw_armstrong() {
+    console.log('drawing...')
+    // https://maker.js.org/docs/basic-drawing/#It%27s%20Just%20JSON
+    var makerjs = require('makerjs');
 
-    var data = null;
-    var file = e.target.files[0];
-    // var file = "test.csv";
+    var model = {
+        models: {
+            ring1: new makerjs.models.Ring(40, 100),
+            bc1: new makerjs.models.BoltCircle(90, 4, 10),
+            bc2: new makerjs.models.BoltCircle(55, 7, 6, 30)
+        }
+    };
 
-    console.log(file);
+    var svg = makerjs.exporter.toSVG(model);
+    DXF = makerjs.exporter.toDXF(model);
+    // console.log(DXF)
+    return svg
 
-    var reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function (event) {
-        var csvData = event.target.result;
 
-        var parsedCSV = d3.csv.parseRows(csvData);
-
-        parsedCSV.forEach(function (d, i) {
-            if (i == 0) return true; // skip the header
-            if (d.constructor === Array) {
-                // console.log(d);
-                // document.getElementById(d[0]).value = d[1];
-                createForm(d);
-            }
-        });
-
-    }
 }
 
-function createForm(csv) {
-    // console.log(out);
-
-    for (var i in csv) {
-        out += '<input value="' + csv[i] + '">';
-    }
-    document.getElementById('csvForm').innerHTML = out;
-    out += '<br>';
-}
-
-function download(e) {
-
+function display() {
     data = [["id", "value"]];
 
-    var f = d3.selectAll("#csvForm > input")[0];
+    var f_value = d3.selectAll("#csvForm > input")[0];
+    var f_id = d3.selectAll("#csvForm > label")[0];
+    for (var i = 0; i < f_value.length; i++) {
+        data.push([f_id[i].textContent, f_value[i].value]);
+    }
+    // console.log(data);
+    if (method == 'armstrong') {
+        console.log('identified as armstrong')
+        document.getElementById('canvas').innerHTML = draw_armstrong();
 
-    f.forEach(function (d, i) {
-        data.push([d.id, d.value]);
-    });
+        
+    }
 
-    console.log(data);
+    let element = document.getElementById("download");
+    // let hidden = element.getAttribute("hidden");
+
+    // if (hidden) {
+    //     element.removeAttribute("hidden");
+    //  } else {
+    //     element.setAttribute("hidden", "hidden");
+    //  }
+    element.removeAttribute("hidden");
+}
+
+function download() {
+
+    data = DXF
 
     var csvContent = "data:text/csv;charset=utf-8,";
-    data.forEach(function (d, i) {
-        dataString = d.join(",");
-        csvContent += i < data.length ? dataString + "\n" : dataString;
-    });
+    // data.forEach(function (d, i) {
+    //     dataString = d.join("");
+    //     csvContent += i < data.length ? dataString + "\n" : dataString;
+    // });
+    csvContent += String(data);
 
     var encodedUri = encodeURI(csvContent);
     var link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "FormData.csv");
+    link.setAttribute("download", "patterns.dxf");
     link.click();
+//     console.log(data)
+//     var element = document.createElement('a');
+//     element.setAttribute('href', 'data:text/plain;charset=utf-8,', + encodeURIComponent(data));
+//     element.setAttribute('download', "patterns.dxf");
+
+//     element.style.display = 'none';
+//     document.body.appendChild(element);
+
+//     element.click();
+
+//     document.body.removeChild(element);
 }
